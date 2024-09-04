a6d10ec59c3cc3219da1054ff75c919a3cc8058f
Shivang Sharma <aad9805066254@gmail.com>
2024-09-04 22:08:55 +0530

    added fluent ui theme

--- Bess/include/settings/themes.h
+++ Bess/include/settings/themes.h
@@ -1,26 +1,28 @@
 #pragma once
 
+#include <functional>
 #include <string>
 #include <unordered_map>
-#include <functional>
 
 namespace Bess::Config {
     class Themes {
-    public:
+      public:
         Themes();
 
-        void applyTheme(const std::string& theme);
-        void addTheme(const std::string& name, const std::function<void()>& callback);
-        const std::unordered_map<std::string, std::function<void()>>& getThemes() const;
-    private:
+        void applyTheme(const std::string &theme);
+        void addTheme(const std::string &name, const std::function<void()> &callback);
+        const std::unordered_map<std::string, std::function<void()>> &getThemes() const;
+
+      private:
         static void setModernColors();
         static void setMaterialYouColors();
         static void setDarkThemeColors();
         static void setBessDarkThemeColors();
         static void setCatpuccinMochaColors();
         static void setGlassTheme();
+        static void setFluentUITheme();
 
         // theme name and a void callback
         std::unordered_map<std::string, std::function<void()>> m_themes = {};
     };
-}
+} // namespace Bess::Config
--- Bess/src/settings/settings.cpp
+++ Bess/src/settings/settings.cpp
@@ -6,66 +6,59 @@ namespace Bess::Config {
         return m_currentTheme;
     }
 
-    const Themes& Settings::getThemes()
-    {
+    const Themes &Settings::getThemes() {
         return m_themes;
     }
 
-    void Settings::loadCurrentTheme()
-    {
+    void Settings::loadCurrentTheme() {
         m_themes.applyTheme(m_currentTheme);
     }
 
-    void Settings::applyTheme(const std::string& theme)
-    {
-        if(m_currentTheme == theme) return;
+    void Settings::applyTheme(const std::string &theme) {
+        if (m_currentTheme == theme)
+            return;
         m_currentTheme = theme;
         m_themes.applyTheme(theme);
     }
 
-    float Settings::getFontSize()
-    {
+    float Settings::getFontSize() {
         return m_fontSize;
     }
 
-    void Settings::setFontSize(float size)
-    {
-        if(m_fontRebuild) return;
+    void Settings::setFontSize(float size) {
+        if (m_fontRebuild)
+            return;
         m_fontSize = size;
         m_fontRebuild = true;
     }
 
-    float Settings::getScale()
-    {
+    float Settings::getScale() {
         return m_scale;
     }
 
-    void Settings::setScale(float scale)
-    {
-        if (m_fontRebuild) return;
+    void Settings::setScale(float scale) {
+        if (m_fontRebuild)
+            return;
         m_scale = scale;
         m_fontRebuild = true;
     }
 
-    bool Settings::shouldFontRebuild()
-    {
+    bool Settings::shouldFontRebuild() {
         return m_fontRebuild;
     }
 
-    void Settings::setFontRebuild(bool rebuild)
-    {
+    void Settings::setFontRebuild(bool rebuild) {
         m_fontRebuild = !rebuild;
     }
 
-    void Settings::init()
-    {
+    void Settings::init() {
         m_themes = Themes();
-        m_currentTheme = "Bess Dark";
+        m_currentTheme = "Fluent UI";
     }
 
     std::string Settings::m_currentTheme;
     Themes Settings::m_themes;
-    float Settings::m_scale = 1.1f;
-    float Settings::m_fontSize = 16.f;
+    float Settings::m_scale = 1.f;
+    float Settings::m_fontSize = 18.f;
     bool Settings::m_fontRebuild = false;
-}
+} // namespace Bess::Config
