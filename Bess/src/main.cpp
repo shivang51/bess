913d046d84434f22daa5c5e4407fcde0b236881e
Shivang Sharma <aad9805066254@gmail.com>
2024-09-15 20:48:27 +0530

    added better termination handeling

--- Bess/src/main.cpp
+++ Bess/src/main.cpp
@@ -1,18 +1,41 @@
 #include "application.h"
+#include <csignal>
+#include <cstdlib>
+#include <execinfo.h>
 #include <filesystem>
 #include <iostream>
-
-#include "project_file.h"
-#include "components_manager/component_bank.h"
+#include <unistd.h>
 
 static bool isValidStartDir() {
     auto cwd = std::filesystem::current_path();
     return std::filesystem::exists(std::filesystem::path(cwd.string() + "/assets"));
 }
 
-int main(int argc, char** argv) {
+static std::string binary;
+
+void print_stacktrace() {
+    void *array[10];
+    size_t size = backtrace(array, 10);
+    char **symbols = backtrace_symbols(array, size);
+    for (size_t i = 0; i < size; ++i) {
+        std::cerr << symbols[i] << std::endl;
+        // Extract the address and resolve it using addr2line
+        char command[256];
+        snprintf(command, sizeof(command), "addr2line -e %s %p", binary.c_str(), array[i]);
+        system(command); // Run addr2line for each address
+    }
+    free(symbols);
+}
+
+void signal_handler(int sig) {
+    std::cerr << "Error: signal " << sig << std::endl;
+    print_stacktrace();
+    exit(1);
+}
+
+int main(int argc, char **argv) {
     std::vector<std::string> args(argv, argv + argc);
-    
+
     if (!isValidStartDir()) {
         std::filesystem::current_path(std::filesystem::path(args[0]).parent_path());
         if (!isValidStartDir()) {
@@ -21,21 +44,22 @@ int main(int argc, char** argv) {
         }
     }
 
-    //Bess::ProjectFile file("dummy.bproj");
-    //file.getName();
-    //Bess::Simulator::ComponentBank::loadFromJson("assets/gates_collection.json");
-
-    //Bess::Simulator::ComponentsManager::generateComponent(Bess::Simulator::ComponentType::inputProbe);
-    //Bess::Simulator::ComponentsManager::generateComponent(Bess::Simulator::ComponentType::outputProbe);
-    //Bess::Simulator::ComponentsManager::generateComponent(Bess::Simulator::ComponentType::jcomponent, Bess::Simulator::ComponentBank::getJCompData("Digital Gates", "NAND Gate"));
-
-    //Bess::ProjectFile file;
-    //file.setName("Test Project");
-    //file.setPath("./test.bproj");
-    //file.save();
+#ifndef NDEBUG
+    binary = args[0];
+    std::cout << "[+] Debug mode for " << binary << std::endl;
+    struct sigaction sa;
+    sa.sa_handler = signal_handler;
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = SA_RESTART;
+    sigaction(SIGSEGV, &sa, nullptr);
+#endif
 
     Bess::Application app = Bess::Application();
-    //app.loadProject("test.bproj");
-    app.run();
+    try {
+        app.run();
+    } catch (const std::exception &e) {
+        std::cerr << e.what() << std::endl;
+        app.quit();
+    }
     return 0;
 }
