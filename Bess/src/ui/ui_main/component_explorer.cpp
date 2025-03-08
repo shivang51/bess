f7a10ff17fa06265cc886deadaec19928cfc0640
Shivang Sharma <aad9805066254@gmail.com>
2025-03-08 23:49:27 +0530

    removing one space for format it correctly

--- Bess/src/ui/ui_main/component_explorer.cpp
+++ Bess/src/ui/ui_main/component_explorer.cpp
@@ -68,7 +68,7 @@ namespace Bess::UI {
             break;
         }
 
-        return std::string(" ") + Icons::FontAwesomeIcons::FA_CUBE + "  ";
+        return std::string(" ") + Icons::FontAwesomeIcons::FA_CUBE + " ";
     }
 
     void ComponentExplorer::draw() {
