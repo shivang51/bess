ea717dad8338f636904423ccad966cd388aaa788
Shivang Sharma <aad9805066254@gmail.com>
2025-05-07 22:15:02 +0530

    minor refactoring

--- Bess/src/main.cpp
+++ Bess/src/main.cpp
@@ -7,14 +7,10 @@
 #ifdef __linux__
     #include <stacktrace>
 
-void print_stacktrace() {
+void signalHandler(int sig) {
+    std::cerr << "Error: signal " << sig << std::endl;
     std::stacktrace st = std::stacktrace::current(10);
     std::cout << st << std::endl;
-}
-
-void signal_handler(int sig) {
-    std::cerr << "Error: signal " << sig << std::endl;
-    print_stacktrace();
     exit(1);
 }
 
@@ -41,7 +37,7 @@ int main(int argc, char **argv) {
 #ifdef __linux__
     #ifndef NDEBUG
     struct sigaction sa;
-    sa.sa_handler = signal_handler;
+    sa.sa_handler = signalHandler;
     sigemptyset(&sa.sa_mask);
     sa.sa_flags = SA_RESTART;
     sigaction(SIGSEGV, &sa, nullptr);
