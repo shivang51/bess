ac34611ce713a08159f7bab83aebd6af5c587a7c
shivang51 <sharma.shivang.608@gmail.com>
2025-07-20 12:47:28 +0530

    moved json covert helpers to separate file

--- Bess/include/scene/components/components.h
+++ Bess/include/scene/components/components.h
@@ -1,81 +1,18 @@
 #pragma once
 
 #include "bess_uuid.h"
+#include "json_convert_helpers.h"
 #include "entt/entity/entity.hpp"
 #include "entt/entity/fwd.hpp"
 #include "json.hpp"
 #include <cstdint>
 #include <vector>
 #define GLM_ENABLE_EXPERIMENTAL
-#include "ext/vector_float3.hpp"
-#include "ext/vector_float4.hpp"
-#include "fwd.hpp"
+#include "glm.hpp"
 #include "gtx/matrix_decompose.hpp"
 #include "simulation_engine.h"
 #include <glm.hpp>
 #include <string>
-
-namespace nlohmann {
-    inline void to_json(nlohmann::json &j, const Bess::UUID &uuid) {
-        j = (uint64_t)uuid;
-    }
-    inline void from_json(const nlohmann::json &j, Bess::UUID &uuid) {
-        uuid = j.get<uint64_t>();
-    }
-
-    inline void to_json(nlohmann::json &j, const glm::vec2 &vec) {
-        j = nlohmann::json{vec.x, vec.y};
-    }
-    inline void from_json(const nlohmann::json &j, glm::vec2 &vec) {
-        vec.x = j.at(0).get<float>();
-        vec.y = j.at(1).get<float>();
-    }
-
-    // glm::vec3
-    inline void to_json(nlohmann::json &j, const glm::vec3 &vec) {
-        j = nlohmann::json{vec.x, vec.y, vec.z};
-    }
-    inline void from_json(const nlohmann::json &j, glm::vec3 &vec) {
-        vec.x = j.at(0).get<float>();
-        vec.y = j.at(1).get<float>();
-        vec.z = j.at(2).get<float>();
-    }
-
-    // glm::vec4
-    inline void to_json(nlohmann::json &j, const glm::vec4 &vec) {
-        j = nlohmann::json{vec.x, vec.y, vec.z, vec.w};
-    }
-    inline void from_json(const nlohmann::json &j, glm::vec4 &vec) {
-        vec.x = j.at(0).get<float>();
-        vec.y = j.at(1).get<float>();
-        vec.z = j.at(2).get<float>();
-        vec.w = j.at(3).get<float>();
-    }
-
-    // glm::mat4 (stored as an array of 16 floats in column-major order)
-    inline void to_json(nlohmann::json &j, const glm::mat4 &mat) {
-        std::vector<float> vals;
-        vals.reserve(16);
-        // GLM matrices are column-major. We store row-major for simplicity.
-        for (int row = 0; row < 4; ++row) {
-            for (int col = 0; col < 4; ++col) {
-                vals.push_back(mat[col][row]);
-            }
-        }
-        j = vals;
-    }
-    inline void from_json(const nlohmann::json &j, glm::mat4 &mat) {
-        std::vector<float> vals = j.get<std::vector<float>>();
-        if (vals.size() != 16)
-            throw std::runtime_error("Invalid matrix size");
-        for (int row = 0; row < 4; ++row) {
-            for (int col = 0; col < 4; ++col) {
-                mat[col][row] = vals[row * 4 + col];
-            }
-        }
-    }
-
-} // namespace nlohmann
 
 namespace Bess::Canvas::Components {
     // IdComponent
@@ -363,5 +300,29 @@ namespace Bess::Canvas::Components {
         comp.clockBhaviour = j.at("clockBhaviour").get<bool>();
         comp.frequencyUnit = j.at("frequencyUnit").get<SimEngine::FrequencyUnit>();
         comp.frequency = j.at("frequency").get<float>();
+    }
+
+
+    // TextNodeComponent
+    class TextNodeComponent {
+        public:
+            TextNodeComponent() = default;
+            TextNodeComponent(const TextNodeComponent &other) = default;
+    
+            std::string text = "";
+            glm::vec4 color = {1.f, 1.f, 1.f, 1.f};
+            float fontSize = 16.f;
+    };
+
+    inline void to_json(nlohmann::json &j, const TextNodeComponent &comp) {
+        j = nlohmann::json{
+            {"text", comp.text},
+            {"color", comp.color},
+            {"fontSize", comp.fontSize}};
+    }
+    inline void from_json(const nlohmann::json &j, TextNodeComponent &comp) {
+        comp.text = j.at("text").get<std::string>();
+        comp.color = j.at("color").get<glm::vec4>();
+        comp.fontSize = j.at("fontSize").get<float>();
     }
 } // namespace Bess::Canvas::Components
