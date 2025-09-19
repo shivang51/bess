7136d65b2eaa574df0049dbed2f6debaf383a02c
Shivang Sharma <aad9805066254@gmail.com>
2025-09-19 21:50:14 +0530

    added basic tests

--- BessTests/main.cpp
+++ BessTests/main.cpp
@@ -1,7 +1,31 @@
+#include "bess_uuid.h"
+#include "simulation_engine.h"
 #include <gtest/gtest.h>
 
-TEST(DummyTest, ShouldPass) {
-    EXPECT_EQ(1, 1);
+class SimulationEngineTest : public testing::Test {
+  protected:
+    SimulationEngineTest() {}
+
+    ~SimulationEngineTest() override {}
+
+    Bess::SimEngine::SimulationEngine simEngine_;
+};
+
+TEST_F(SimulationEngineTest, MinorConfig) {
+    ASSERT_EQ(simEngine_.getSimulationState(), Bess::SimEngine::SimulationState::running);
+    simEngine_.toggleSimState();
+    ASSERT_EQ(simEngine_.getSimulationState(), Bess::SimEngine::SimulationState::paused);
+    simEngine_.toggleSimState();
+    ASSERT_EQ(simEngine_.getSimulationState(), Bess::SimEngine::SimulationState::running);
+}
+
+TEST_F(SimulationEngineTest, AddingRemovingComponents) {
+    auto uuid = simEngine_.addComponent(Bess::SimEngine::ComponentType::AND, 2);
+    EXPECT_NE(uuid, Bess::UUID::null);
+    uuid = simEngine_.addComponent(Bess::SimEngine::ComponentType::AND, 3);
+    EXPECT_NE(uuid, Bess::UUID::null);
+    uuid = simEngine_.addComponent(Bess::SimEngine::ComponentType::AND, 4);
+    EXPECT_NE(uuid, Bess::UUID::null);
 }
 
 int main(int argc, char **argv) {
