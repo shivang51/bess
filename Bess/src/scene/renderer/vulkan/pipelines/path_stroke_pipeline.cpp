ec5ed9851333687620773bbeed2f8b0ab77b16ee
Shivang Sharma <aad9805066254@gmail.com>
2025-10-10 21:12:46 +0530

    fixed minor bug

--- Bess/src/scene/renderer/vulkan/pipelines/path_stroke_pipeline.cpp
+++ Bess/src/scene/renderer/vulkan/pipelines/path_stroke_pipeline.cpp
@@ -204,7 +204,7 @@ namespace Bess::Vulkan::Pipelines {
 
         VkPipelineInputAssemblyStateCreateInfo inputAssembly{};
         inputAssembly.sType = VK_STRUCTURE_TYPE_PIPELINE_INPUT_ASSEMBLY_STATE_CREATE_INFO;
-        inputAssembly.topology = VK_PRIMITIVE_TOPOLOGY_TRIANGLE_FAN;
+        inputAssembly.topology = VK_PRIMITIVE_TOPOLOGY_TRIANGLE_STRIP;
         inputAssembly.primitiveRestartEnable = VK_TRUE;
 
         auto viewportState = createViewportState();
