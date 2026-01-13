3be3f8fe70bd4ccc4b9c461c93082b6cd31f30a8
Shivang Sharma <aad9805066254@gmail.com>
2026-01-13 14:49:29 +0530

    improved toggle button ui

--- src/Bess/scene/scene_ui/scene_ui.h
+++ src/Bess/scene/scene_ui/scene_ui.h
@@ -11,13 +11,18 @@ namespace Bess::Canvas::SceneUI {
                           const std::shared_ptr<Renderer::MaterialRenderer> &materialRenderer,
                           const std::shared_ptr<Renderer2D::Vulkan::PathRenderer> &pathRenderer) {
 
-        constexpr Renderer::QuadRenderProperties buttonProps{.borderRadius = glm::vec4(4.f)};
+        static const Renderer::QuadRenderProperties trackProps{
+            .borderColor = ViewportTheme::colors.componentBorder,
+            .borderRadius = glm::vec4(5.5f),
+            .borderSize = glm::vec4(0.5f),
+        };
+        constexpr Renderer::QuadRenderProperties buttonProps{.borderRadius = glm::vec4(5.f)};
         // Button background / track
         materialRenderer->drawQuad(buttonPos,
                                    buttonSize,
                                    isHigh ? ViewportTheme::colors.stateHigh : ViewportTheme::colors.background,
                                    id,
-                                   buttonProps);
+                                   trackProps);
 
         // Toggle head
         const float buttonHeadPosX = isHigh
