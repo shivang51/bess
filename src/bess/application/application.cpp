3d2f2fe04e1253ce3e95835be0909bb7fb07971f
Shivang Sharma <aad9805066254@gmail.com>
2026-05-27 21:33:44 +0530

    temp testing

--- src/bess/application/application.cpp
+++ src/bess/application/application.cpp
@@ -1,9 +1,13 @@
 #include "application.h"
 #include "asset_manager/asset_manager.h"
+#include "assets.h"
 #include "bess_core/g_app_context.h"
 #include "bess_core/project_context.h"
 #include "bess_core/renderer/renderer_2d.h"
+#include "bess_core/renderer/renderer_types.h"
+#include "bess_core/renderer/texture.h"
 #include "bess_wgpu/wgpu_renderer_2d.h"
+#include "bess_wgpu/wgpu_texture.h"
 #include "common/bess_assert.h"
 #include "common/logger.h"
 #include "common/types.h"
@@ -20,6 +24,7 @@
 
 #include "application/window.h"
 #include "settings/settings.h"
+#include "webgpu/webgpu.h"
 
 namespace Bess {
     Application::Application() = default;
@@ -30,21 +35,42 @@ namespace Bess {
         Bess::Wgpu::WgpuRenderer2D renderer2D;
         Core::Renderer::Renderer2DNativeSurfaceType surfaceType =
             Core::Renderer::Renderer2DNativeSurfaceType::BackendOwned;
+
         renderer2D.init(
             {.extent = {800, 600},
              .targetFormat = Core::Renderer::Renderer2DTargetFormat::BGRA8Unorm,
              .surface = {.type = surfaceType, .handle = nullptr}});
 
+        auto tex = Wgpu::WgpuTexture(renderer2D,
+                                     "assets/images/7-seg-display-tilemap.png");
+        tex.init();
+
         renderer2D.beginFrame({.extent = {800, 600},
                                .clearColor = {0.1f, 0.2f, 0.3f, 1.0f},
                                .shouldClear = true});
-        renderer2D.clear({0.2f, 0.8f, 0.4f, 1.f});
 
+        Core::Renderer::QuadProps quadProps;
+        quadProps.position = {100.f, 100.f};
+        quadProps.size = {200.f, 150.f};
+        quadProps.color = Bess::Core::Renderer::Color{1.f, 0.f, 0.f, 1.f};
+        quadProps.rotation = 45.f;
         renderer2D.drawQuad({.position = {100.f, 100.f},
                              .size = {200.f, 150.f},
                              .color = {1.f, 0.f, 0.f, 1.f}});
 
         renderer2D.endFrame();
+
+        renderer2D.beginFrame({.extent = {800, 600},
+                               .clearColor = {0.1f, 0.2f, 0.3f, 1.0f},
+                               .shouldClear = false});
+
+        renderer2D.drawQuad({.position = {300.f, 100.f},
+                             .size = {200.f, 150.f},
+                             .color = {1.f, 1.f, 1.f, 1.f},
+                             .texture = tex.getHandle()});
+
+        renderer2D.endFrame();
+
         renderer2D.saveTargetToFile("output.png");
 
         renderer2D.destroy();
