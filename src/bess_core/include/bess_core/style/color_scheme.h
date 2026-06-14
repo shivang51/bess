9f0b931b19dcdc4c8f727f90d769618107faa6f6
Shivang Sharma <aad9805066254@gmail.com>
2026-06-14 18:09:08 +0530

    finished color scheme generation

--- src/bess_core/include/bess_core/style/color_scheme.h
+++ src/bess_core/include/bess_core/style/color_scheme.h
@@ -3,16 +3,18 @@
 // Just trying to create something like Flutter
 // https://github.com/flutter/flutter/blob/master/packages/flutter/lib/src/material/color_scheme.dart
 #include "bess_core/renderer/renderer_types.h"
-#include "color_utils.h"
 #include "common/bess_assert.h"
 #include "common/class_helpers.h"
 #include "cpp/cam/hct.h"
+#include "cpp/dynamiccolor/dynamic_scheme.h"
+#include "cpp/scheme/scheme_tonal_spot.h"
 #include <cstdint>
 
 namespace Bess::Core::Style {
     enum class Brightness : uint8_t { light, dark };
 
     using Color = Renderer::Color;
+    using DynamicScheme = material_color_utilities::DynamicScheme;
 
     struct ColorSchemeColors {
         Color primary;
@@ -68,6 +70,9 @@ namespace Bess::Core::Style {
         Color shadow;
         Color scrim;
         Color surfaceTint;
+
+        static ColorSchemeColors
+        fromDynamicScheme(const DynamicScheme &dynamicScheme);
     };
 
     class ColorScheme {
@@ -85,15 +90,23 @@ namespace Bess::Core::Style {
                  Brightness brightness = Brightness::dark) {
             ColorScheme scheme;
             scheme.m_brightness = brightness;
+
+            const auto dynamicScheme = buildDynamicScheme(
+                seedColor, brightness, scheme.m_contrastLevel);
+
+            scheme.m_colors =
+                ColorSchemeColors::fromDynamicScheme(dynamicScheme);
+
             return scheme;
         }
 
         MAKE_GETTER_SETTER(Brightness, Brightness, m_brightness);
 
       private:
-        static ColorScheme buildDynamicScheme(const Renderer::Color &seedColor,
-                                              const Brightness brightness,
-                                              const float contrast) {
+        static DynamicScheme
+        buildDynamicScheme(const Renderer::Color &seedColor,
+                           const Brightness brightness,
+                           const float contrast) {
 
             BESS_ASSERT(contrast >= -1.0f && contrast <= 1.0f,
                         "Contrast must be between -1.0 and 1.0");
@@ -102,12 +115,12 @@ namespace Bess::Core::Style {
 
             const material_color_utilities::Hct sourceCol(seedColor.toARGB8());
 
-            ColorScheme scheme;
-
-            return scheme;
+            return material_color_utilities::SchemeTonalSpot(
+                sourceCol, isDark, contrast);
         }
 
       private:
+        ColorSchemeColors m_colors;
         Brightness m_brightness{Brightness::dark};
         float m_contrastLevel{0.0f};
     };
--- src/bess_core/src/bess_core/style/color_scheme.cpp
+++ src/bess_core/src/bess_core/style/color_scheme.cpp
@@ -1,3 +1,94 @@
 #include "bess_core/style/color_scheme.h"
 
-namespace Bess::Core::Style {} // namespace Bess::Core::Style
+namespace Bess::Core::Style {
+    ColorSchemeColors
+    ColorSchemeColors::fromDynamicScheme(const DynamicScheme &dynamicScheme) {
+        ColorSchemeColors colors;
+        colors.primary = Color::FromARGB(dynamicScheme.GetPrimary());
+        colors.onPrimary = Color::FromARGB(dynamicScheme.GetOnPrimary());
+        colors.primaryContainer =
+            Color::FromARGB(dynamicScheme.GetPrimaryContainer());
+        colors.onPrimaryContainer =
+            Color::FromARGB(dynamicScheme.GetOnPrimaryContainer());
+        colors.primaryFixed = Color::FromARGB(dynamicScheme.GetPrimaryFixed());
+        colors.primaryFixedDim =
+            Color::FromARGB(dynamicScheme.GetPrimaryFixedDim());
+        colors.onPrimaryFixed =
+            Color::FromARGB(dynamicScheme.GetOnPrimaryFixed());
+        colors.onPrimaryFixedVariant =
+            Color::FromARGB(dynamicScheme.GetOnPrimaryFixedVariant());
+
+        colors.secondary = Color::FromARGB(dynamicScheme.GetSecondary());
+        colors.onSecondary = Color::FromARGB(dynamicScheme.GetOnSecondary());
+        colors.secondaryContainer =
+            Color::FromARGB(dynamicScheme.GetSecondaryContainer());
+        colors.onSecondaryContainer =
+            Color::FromARGB(dynamicScheme.GetOnSecondaryContainer());
+        colors.secondaryFixed =
+            Color::FromARGB(dynamicScheme.GetSecondaryFixed());
+        colors.secondaryFixedDim =
+            Color::FromARGB(dynamicScheme.GetSecondaryFixedDim());
+        colors.onSecondaryFixed =
+            Color::FromARGB(dynamicScheme.GetOnSecondaryFixed());
+        colors.onSecondaryFixedVariant =
+            Color::FromARGB(dynamicScheme.GetOnSecondaryFixedVariant());
+
+        colors.tertiary = Color::FromARGB(dynamicScheme.GetTertiary());
+        colors.onTertiary = Color::FromARGB(dynamicScheme.GetOnTertiary());
+        colors.tertiaryContainer =
+            Color::FromARGB(dynamicScheme.GetTertiaryContainer());
+        colors.onTertiaryContainer =
+            Color::FromARGB(dynamicScheme.GetOnTertiaryContainer());
+        colors.tertiaryFixed =
+            Color::FromARGB(dynamicScheme.GetTertiaryFixed());
+        colors.tertiaryFixedDim =
+            Color::FromARGB(dynamicScheme.GetTertiaryFixedDim());
+        colors.onTertiaryFixed =
+            Color::FromARGB(dynamicScheme.GetOnTertiaryFixed());
+        colors.onTertiaryFixedVariant =
+            Color::FromARGB(dynamicScheme.GetOnTertiaryFixedVariant());
+
+        colors.error = Color::FromARGB(dynamicScheme.GetError());
+        colors.onError = Color::FromARGB(dynamicScheme.GetOnError());
+        colors.errorContainer =
+            Color::FromARGB(dynamicScheme.GetErrorContainer());
+        colors.onErrorContainer =
+            Color::FromARGB(dynamicScheme.GetOnErrorContainer());
+
+        colors.outline = Color::FromARGB(dynamicScheme.GetOutline());
+        colors.outlineVariant =
+            Color::FromARGB(dynamicScheme.GetOutlineVariant());
+
+        colors.surface = Color::FromARGB(dynamicScheme.GetSurface());
+        colors.onSurface = Color::FromARGB(dynamicScheme.GetOnSurface());
+        colors.surfaceDim = Color::FromARGB(dynamicScheme.GetSurfaceDim());
+        colors.surfaceBright =
+            Color::FromARGB(dynamicScheme.GetSurfaceBright());
+        colors.surfaceContainerLowest =
+            Color::FromARGB(dynamicScheme.GetSurfaceContainerLowest());
+        colors.surfaceContainerLow =
+            Color::FromARGB(dynamicScheme.GetSurfaceContainerLow());
+        colors.surfaceContainer =
+            Color::FromARGB(dynamicScheme.GetSurfaceContainer());
+        colors.surfaceContainerHigh =
+            Color::FromARGB(dynamicScheme.GetSurfaceContainerHigh());
+        colors.surfaceContainerHighest =
+            Color::FromARGB(dynamicScheme.GetSurfaceContainerHighest());
+        colors.onSurfaceVariant =
+            Color::FromARGB(dynamicScheme.GetOnSurfaceVariant());
+
+        colors.inverseSurface =
+            Color::FromARGB(dynamicScheme.GetInverseSurface());
+        colors.onInverseSurface =
+            Color::FromARGB(dynamicScheme.GetInverseOnSurface());
+        colors.inversePrimary =
+            Color::FromARGB(dynamicScheme.GetInversePrimary());
+
+        colors.shadow = Color::FromARGB(dynamicScheme.GetShadow());
+        colors.scrim = Color::FromARGB(dynamicScheme.GetScrim());
+        colors.surfaceTint = Color::FromARGB(dynamicScheme.GetSurfaceTint());
+
+        return colors;
+    }
+
+} // namespace Bess::Core::Style
