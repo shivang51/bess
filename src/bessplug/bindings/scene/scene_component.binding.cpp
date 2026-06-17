a935a12e7b897c98dce714a86db89d4553dc30b1
Shivang Sharma <aad9805066254@gmail.com>
2026-06-17 17:30:56 +0530

    fixed compilation issues

--- src/bessplug/bindings/scene/scene_component.binding.cpp
+++ src/bessplug/bindings/scene/scene_component.binding.cpp
@@ -1,8 +1,7 @@
 #include "bess_core/renderer/renderer_2d.h"
-#include "camera.h"
+#include "bess_core/scene/camera.h"
 #include "bess_core/scene/scene_draw_context.h"
 #include "bess_core/scene/scene_state/scene_state.h" // included for pybind11
-#include "bess_core/scene/scene_draw_context.h"
 #include <pybind11/functional.h>
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
