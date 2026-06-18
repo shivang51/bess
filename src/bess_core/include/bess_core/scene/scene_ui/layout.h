c854a88db01d6ab6b1a1ac5b58e6ca9ac1cf42e2
Shivang Sharma <aad9805066254@gmail.com>
2026-06-18 19:47:09 +0530

    added some tests

--- src/bess_core/include/bess_core/scene/scene_ui/layout.h
+++ src/bess_core/include/bess_core/scene/scene_ui/layout.h
@@ -1,8 +1,10 @@
 #pragma once
 
 #include "common/bess_api.h"
+#include "common/bess_assert.h"
 #include "common/bess_uuid.h"
 #include "common/class_helpers.h"
+#include "common/logger.h"
 #include "common/types.h"
 #include "ext/vector_float2.hpp"
 #include <cstdint>
@@ -49,34 +51,50 @@ namespace Bess::Canvas::UI {
       public:
         UINode() = default;
 
+        void setPosDirty(bool dirty = true) {
+            m_posDirty = dirty;
+        }
+
+        void setSizeDirty(bool dirty = true) {
+            m_sizeDirty = dirty;
+        }
+
         MAKE_GETTER_SETTER(UUID, Id, m_id);
-        MAKE_GETTER_SETTER(glm::vec2, Pos, m_pos);
-        MAKE_GETTER_SETTER(Unit, PosUnit, m_posUnit);
-        MAKE_GETTER_SETTER(glm::vec2, Size, m_size);
-        MAKE_GETTER_SETTER(Unit, SizeUnit, m_sizeUnit);
+        MAKE_GETTER_SETTER_WC(glm::vec2, Pos, m_pos, setPosDirty);
+        MAKE_GETTER_SETTER_WC(Unit, PosUnit, m_posUnit, setPosDirty);
+        MAKE_GETTER_SETTER_WC(glm::vec2, Size, m_size, setSizeDirty);
+        MAKE_GETTER_SETTER_WC(Unit, SizeUnit, m_sizeUnit, setSizeDirty);
         MAKE_GETTER_SETTER(LayoutDirection, Direction, m_direction);
         MAKE_GETTER_SETTER(LayoutAlignment, Alignment, m_alignment);
         MAKE_GETTER_SETTER(PosMode, PosMode, m_posMode);
-        MAKE_GETTER_SETTER(SizeContraint, SizeConstraint, m_sizeConstraint);
+        MAKE_GETTER_SETTER_WC(SizeContraint,
+                              SizeConstraint,
+                              m_sizeConstraint,
+                              setSizeDirty);
         MAKE_GETTER_SETTER(OrderedSet<UUID>, Children, m_children);
         MAKE_GETTER_SETTER(bool, PosDirty, m_posDirty);
         MAKE_GETTER_SETTER(bool, SizeDirty, m_sizeDirty);
         MAKE_GETTER_SETTER(glm::vec2, CachedPos, m_cachedPos);
         MAKE_GETTER_SETTER(glm::vec2, CachedSize, m_cachedSize);
         MAKE_GETTER_SETTER(float, ZVal, zVal);
-        MAKE_GETTER_SETTER(glm::vec4, Padding, m_padding);
-        MAKE_GETTER_SETTER(glm::vec4, Margin, m_margin);
+        MAKE_GETTER_SETTER_WC(glm::vec4, Padding, m_padding, setSizeDirty);
+        MAKE_GETTER_SETTER_WC(glm::vec4, Margin, m_margin, setSizeDirty);
+        MAKE_GETTER_SETTER_WC(glm::vec2, MinSize, m_minSize, setSizeDirty);
+        MAKE_GETTER_SETTER_WC(glm::vec2, MaxSize, m_maxSize, setSizeDirty);
 
         void addChild(const UUID &childId) {
             m_children.insert(childId);
+            m_sizeDirty = true;
         }
 
         void removeChild(const UUID &childId) {
             m_children.erase(childId);
+            m_sizeDirty = true;
         }
 
         void clearChildren() {
             m_children.clear();
+            m_sizeDirty = true;
         }
 
         glm::vec2 measure(UINodeRegistry &registry, const UUID &parentId) {
@@ -84,11 +102,19 @@ namespace Bess::Canvas::UI {
                 return m_cachedSize;
             }
 
+            auto parentNode = registry.getNode(parentId);
+
+            glm::vec2 size = {};
             if (m_sizeConstraint == SizeContraint::fixed) {
-                return m_sizeUnit == Unit::pixel
-                           ? m_size
-                           : m_size *
-                                 registry.getNode(parentId)->getCachedSize();
+                BESS_ASSERT(m_sizeUnit != Unit::relative ||
+                                parentNode != nullptr,
+                            "Relative size requires a parent node.");
+
+                if (!parentNode || m_sizeUnit == Unit::pixel) {
+                    size = m_size;
+                } else {
+                    size = m_size * registry.getNode(parentId)->getCachedSize();
+                }
             }
 
             glm::vec2 childrenSpan{0.f};
@@ -96,49 +122,68 @@ namespace Bess::Canvas::UI {
                 UINode *childNode = registry.getNode(childId);
                 if (childNode) {
                     const auto childSize = childNode->measure(registry, m_id);
-                    if (m_direction == LayoutDirection::horizontal) {
-                        childrenSpan.x += childSize.x;
-                        childrenSpan.y = std::max(childrenSpan.y, childSize.y);
-                    } else {
-                        childrenSpan.y += childSize.y;
-                        childrenSpan.x = std::max(childrenSpan.x, childSize.x);
+                    if (m_sizeConstraint != SizeContraint::fixed) {
+                        if (m_direction == LayoutDirection::horizontal) {
+                            childrenSpan.x += childSize.x;
+                            childrenSpan.y =
+                                std::max(childrenSpan.y, childSize.y);
+                        } else {
+                            childrenSpan.y += childSize.y;
+                            childrenSpan.x =
+                                std::max(childrenSpan.x, childSize.x);
+                        }
                     }
                 }
             }
 
-            glm::vec2 totalSize =
-                childrenSpan +
-                glm::vec2(m_padding.x + m_padding.z, m_padding.y + m_padding.w);
-            totalSize +=
-                glm::vec2(m_margin.x + m_margin.z, m_margin.y + m_margin.w);
+            if (m_sizeConstraint != SizeContraint::fixed) {
+                size =
+                    childrenSpan +
+                    glm::vec2(m_padding.x + m_padding.z,
+                              m_padding.y + m_padding.w) +
+                    glm::vec2(m_margin.x + m_margin.z, m_margin.y + m_margin.w);
+            }
 
             if (m_maxSize.x >= 0.f)
-                totalSize.x = std::min(totalSize.x, m_maxSize.x);
+                size.x = std::min(size.x, m_maxSize.x);
             if (m_maxSize.y >= 0.f)
-                totalSize.y = std::min(totalSize.y, m_maxSize.y);
+                size.y = std::min(size.y, m_maxSize.y);
 
+            // Minimum size overrides the max-size if needed
             if (m_minSize.x >= 0.f)
-                totalSize.x = std::max(totalSize.x, m_minSize.x);
+                size.x = std::max(size.x, m_minSize.x);
             if (m_minSize.y >= 0.f)
-                totalSize.y = std::max(totalSize.y, m_minSize.y);
+                size.y = std::max(size.y, m_minSize.y);
 
-            m_cachedSize = totalSize;
+            m_cachedSize = size;
+            m_sizeDirty = false;
+            m_posDirty = true;
 
-            return totalSize;
+            return m_cachedSize;
         }
 
         void layout(UINodeRegistry &registry, const UUID &parentId) {
             if (!m_posDirty) {
+                BESS_TRACE("Node {} position is not dirty, skipping layout.",
+                           (uint64_t)m_id);
                 return;
             }
 
-            glm::vec2 cursor = m_pos - (m_cachedSize / 2.f);
+            if (parentId == UUID::null) {
+                m_cachedPos = m_pos;
+            }
+
+            glm::vec2 cursor = m_cachedPos - (m_cachedSize / 2.f);
             cursor += glm::vec2(m_padding.x, m_padding.y);
 
             for (const auto &childId : m_children) {
                 UINode *childNode = registry.getNode(childId);
 
-                if (!childNode)
+                BESS_ASSERT(childNode,
+                            "Child node {} not found in registry.",
+                            (uint64_t)childId);
+
+                if (childNode == nullptr)
                     continue;
 
                 const glm::vec2 &childMargin = childNode->getMargin();
@@ -146,6 +191,8 @@ namespace Bess::Canvas::UI {
                 childNode->setCachedPos(childPos +
                                         (childNode->getCachedSize() / 2.f));
 
+                const auto childZ = childNode->getZVal();
+                childNode->setZVal(zVal + childZ);
                 childNode->layout(registry, m_id);
 
                 if (m_direction == LayoutDirection::horizontal) {
@@ -154,6 +201,8 @@ namespace Bess::Canvas::UI {
                     cursor.y += childNode->getCachedSize().y;
                 }
             }
+
+            m_posDirty = false;
         }
 
       private:
@@ -179,7 +228,7 @@ namespace Bess::Canvas::UI {
         bool m_posDirty = true;
         bool m_sizeDirty = true;
 
-        glm::vec2 m_cachedPos;
-        glm::vec2 m_cachedSize;
+        glm::vec2 m_cachedPos{0};
+        glm::vec2 m_cachedSize{-1};
     };
 } // namespace Bess::Canvas::UI
--- tests/ui_layout_test.cpp
+++ tests/ui_layout_test.cpp
@@ -1,4 +1,5 @@
 #include "bess_core/scene/scene_ui/layout.h"
+#include "common/bess_uuid.h"
 #include <gtest/gtest.h>
 class UiLayoutTests : public testing::Test {};
 
@@ -15,4 +16,174 @@ TEST_F(UiLayoutTests, UINodeRegistryAddGetRemoveNode) {
     registry.removeNode(node.getId());
     retrievedNode = registry.getNode(node.getId());
     EXPECT_EQ(retrievedNode, nullptr);
+}
+
+TEST_F(UiLayoutTests, UINodeMeasure) {
+    Bess::Canvas::UI::UINodeRegistry registry;
+
+    constexpr glm::vec2 size(100, 50);
+    constexpr glm::vec4 padding(10, 5, 9, 6);
+    constexpr glm::vec4 margin(2, 3, 4, 5);
+    constexpr glm::vec2 wrapContentSize =
+        glm::vec2(padding.x + padding.z, padding.y + padding.w) +
+        glm::vec2(margin.x + margin.z, margin.y + margin.w);
+
+    Bess::Canvas::UI::UINode node;
+    node.setSize(size);
+    EXPECT_EQ(node.getSizeDirty(), true);
+    EXPECT_EQ(node.getSize(), size);
+
+    node.setPadding(padding);
+    EXPECT_EQ(node.getSizeDirty(), true);
+    EXPECT_EQ(node.getPadding(), padding);
+
+    node.setMargin(margin);
+    EXPECT_EQ(node.getSizeDirty(), true);
+    EXPECT_EQ(node.getMargin(), margin);
+
+    auto measuredSize = node.measure(registry, Bess::UUID::null);
+    auto worldSize = node.getCachedSize();
+
+    EXPECT_EQ(measuredSize.x, worldSize.x);
+    EXPECT_EQ(measuredSize.y, worldSize.y);
+
+    // checking for SizeContraint::fixed size
+    EXPECT_EQ(worldSize.x, size.x);
+    EXPECT_EQ(worldSize.y, size.y);
+
+    BESS_INFO("Measured with fixed constraint");
+
+    node.setSizeConstraint(Bess::Canvas::UI::SizeContraint::wrap_content);
+    measuredSize = node.measure(registry, Bess::UUID::null);
+    worldSize = node.getCachedSize();
+
+    // checking for SizeContraint::wrap_content size
+    EXPECT_EQ(measuredSize.x, worldSize.x);
+    EXPECT_EQ(measuredSize.y, worldSize.y);
+
+    EXPECT_EQ(worldSize.x, wrapContentSize.x);
+    EXPECT_EQ(worldSize.y, wrapContentSize.y);
+
+    BESS_INFO("Measured with wrap_content constraint");
+
+    Bess::Canvas::UI::UINode childNode;
+    childNode.setSize(glm::vec2(20, 10));
+    registry.addNode(childNode);
+    node.addChild(childNode.getId());
+
+    measuredSize = node.measure(registry, Bess::UUID::null);
+    worldSize = node.getCachedSize();
+
+    // checking for SizeContraint::wrap_content size with child
+    EXPECT_EQ(measuredSize.x, worldSize.x);
+    EXPECT_EQ(measuredSize.y, worldSize.y);
+
+    EXPECT_EQ(worldSize.x, wrapContentSize.x + 20);
+    EXPECT_EQ(worldSize.y, wrapContentSize.y + 10);
+
+    auto childNodePtr = registry.getNode(childNode.getId());
+    ASSERT_NE(childNodePtr, nullptr);
+    EXPECT_EQ(childNodePtr->getCachedSize().x, 20);
+    EXPECT_EQ(childNodePtr->getCachedSize().y, 10);
+
+    BESS_INFO("Measured with wrap_content constraint and child node");
+
+    node.setMinSize(glm::vec2(150, 100));
+    measuredSize = node.measure(registry, Bess::UUID::null);
+    worldSize = node.getCachedSize();
+
+    // checking for SizeContraint::wrap_content size with child and min size
+    EXPECT_EQ(measuredSize.x, worldSize.x);
+    EXPECT_EQ(measuredSize.y, worldSize.y);
+
+    EXPECT_EQ(worldSize.x, 150);
+    EXPECT_EQ(worldSize.y, 100);
+
+    BESS_INFO("Measured with wrap_content constraint, child node and min size");
+
+    node.setMaxSize(glm::vec2(120, 80));
+    measuredSize = node.measure(registry, Bess::UUID::null);
+    worldSize = node.getCachedSize();
+
+    // checking for SizeContraint::wrap_content size with child, min size and
+    // max size
+    EXPECT_EQ(measuredSize.x, worldSize.x);
+    EXPECT_EQ(measuredSize.y, worldSize.y);
+
+    // Minimum width overrides maximum width
+    EXPECT_EQ(worldSize.x, 150);
+    EXPECT_EQ(worldSize.y, 100);
+
+    node.setMinSize(glm::vec2(50, 40));
+    node.setSize(glm::vec2(200, 150));
+
+    node.setSizeConstraint(Bess::Canvas::UI::SizeContraint::fixed);
+    measuredSize = node.measure(registry, Bess::UUID::null);
+    worldSize = node.getCachedSize();
+
+    EXPECT_EQ(measuredSize.x, worldSize.x);
+    EXPECT_EQ(measuredSize.y, worldSize.y);
+
+    EXPECT_EQ(worldSize.x, 120);
+    EXPECT_EQ(worldSize.y, 80);
+
+    BESS_INFO("Checked min max size overrides");
+}
+
+TEST_F(UiLayoutTests, UINodeLayout) {
+    Bess::Canvas::UI::UINodeRegistry registry;
+
+    Bess::Canvas::UI::UINode parentNode;
+    parentNode.setSize(glm::vec2(200, 100));
+    parentNode.setPos(glm::vec2(0, 0));
+    registry.addNode(parentNode);
+
+    Bess::Canvas::UI::UINode *childNode1Ptr = nullptr;
+    Bess::Canvas::UI::UINode *childNode2Ptr = nullptr;
+    {
+        Bess::Canvas::UI::UINode childNode1;
+        childNode1.setSize(glm::vec2(50, 50));
+        registry.addNode(childNode1);
+        parentNode.addChild(childNode1.getId());
+        childNode1Ptr = registry.getNode(childNode1.getId());
+    }
+
+    {
+        Bess::Canvas::UI::UINode childNode2;
+        childNode2.setSize(glm::vec2(30, 30));
+        registry.addNode(childNode2);
+        parentNode.addChild(childNode2.getId());
+        childNode2Ptr = registry.getNode(childNode2.getId());
+    }
+
+    EXPECT_EQ(parentNode.getAlignment(),
+              Bess::Canvas::UI::LayoutAlignment::start);
+    EXPECT_EQ(parentNode.getDirection(),
+              Bess::Canvas::UI::LayoutDirection::horizontal);
+    EXPECT_EQ(parentNode.getChildren().size(), 2);
+
+    parentNode.measure(registry, Bess::UUID::null);
+
+    glm::vec2 childSize = childNode1Ptr->getCachedSize();
+    EXPECT_EQ(childSize.x, 50);
+    EXPECT_EQ(childSize.y, 50);
+
+    childSize = childNode2Ptr->getCachedSize();
+    EXPECT_EQ(childSize.x, 30);
+    EXPECT_EQ(childSize.y, 30);
+
+    parentNode.layout(registry, Bess::UUID::null);
+
+    auto parentPos = parentNode.getCachedPos();
+
+    auto child1Pos = childNode1Ptr->getCachedPos();
+    auto child2Pos = childNode2Ptr->getCachedPos();
+
+    EXPECT_EQ(child1Pos.x, -75);
+    EXPECT_EQ(child1Pos.y, -25);
+
+    EXPECT_EQ(child2Pos.x, -35);
+    EXPECT_EQ(child2Pos.y, -35);
+
+    BESS_INFO("Checked layout positions of child nodes");
 }
